7a47326e924a2b271440290f5680489a580cc522
joaoito <joaovictorpfr@gmail.com>
2024-10-22 22:02:40 -0300

    uri string mongodb

--- src/app/api/database/connection.ts
+++ src/app/api/database/connection.ts
@@ -6,8 +6,27 @@ if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
+// Extend the NodeJS global type
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
-export async function getMongoClient() {
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export async function getMongoClient(): Promise<MongoClient> {
   try {
     const client = await clientPromise;
     console.log("Conexão ao MongoDB estabelecida com sucesso!");
@@ -17,3 +36,6 @@ export async function getMongoClient() {
     throw error;
   }
 }
+
+// Export a module-scoped MongoClient promise
+export default clientPromise;
